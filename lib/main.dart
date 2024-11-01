cd8f23b2d2ef38f75403aa953f0542a6dead9cbe
‪Amar Yasser‬‏ <amaryasser046@gmail.com>
2024-11-01 23:36:23 +0200

    Add initial Flutter project structure and configuration files

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:chat_app/pages/login_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const ScholerChat());
+}
+
+class ScholerChat extends StatelessWidget {
+  const ScholerChat({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return LoginPage();
+  }
+}
